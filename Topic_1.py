2755732a040aa147825a341f383474ca2c215b02
Mahima0910 <mahimasingh8311@gmail.com>
2025-05-19 03:34:35 +0530

    Add files via upload

---
+++ Topic_1.py
@@ -0,0 +1,26 @@
+print("Tasks:")
+
+# 1. Install Python 3 on your computer. 
+# Already installed.
+
+# 2. Install a code editor of your choice. 
+# Using VS code editor for this.
+
+# 3.Write a simple Python script that prints your name and a greeting.
+name= " Mahima"
+greetings= "Welcome, \nTechSonix Solutions"
+print((greetings) + (name))
+
+# 4. Experiment with running Python code directly in the interpreter.
+# Done
+
+# 5. Write a script with comments explaining different parts of the code.
+name= input("Name: ")
+print(f"Hello, {name}.\nIt's nice to meet you!\nHave a nice day!")
+'''Here we used:
+name as variables,
+input function to take name as input from user,
+print for output,
+'f'  is used as f string to concatenate two things,
+'\n' for adding new lines,
+and triple inverted comma to right multi line codes. '''
